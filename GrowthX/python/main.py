2cb0584a7eea10f8bc309e3be54ad92f8d4715ef
Rishik00 <96735720+Rishik00@users.noreply.github.com>
2024-11-21 17:46:16 +0530

    Done authenticating users for viewing/adding stuff

--- GrowthX/python/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from fastapi import FastAPI
-from user import router as assignment_router
-
-app = FastAPI()
-
-# Include the assignment router
-app.include_router()
-app.include_router(assignment_router)
